d5eae8baa4e8f45656fb60317bd399123551e096
ghostlhq <790492245@qq.com>
2018-05-27 20:34:45 +0000

    更新 'crazytest.py'

--- crazytest.py
+++ crazytest.py
@@ -12,9 +12,11 @@ p = GPIO.PWM(servopin,50)
 p.start(0)
 time.sleep(2)
 
-while(True):
+
   for i in range(0,181,10):
     p.ChangeDutyCycle(2.5 + 10 * i / 180) 
     time.sleep(0.02)                     
     p.ChangeDutyCycle(0)              
     time.sleep(0.2)
+    
+pause()
