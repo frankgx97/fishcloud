17c055607ac35c6e4f870bcacdfbb43bb889f133
ghostlhq <790492245@qq.com>
2018-05-27 15:41:48 +0000

    更新 'test.py'

--- test.py
+++ test.py
@@ -1,8 +1,19 @@
-from gpiozero import LED
-from time import sleep
+import time
+import RPi.GPIO as GPIO  
+GPIO.setmode(GPIO.BOARD) 
+GPIO.setup(12, GPIO.OUT)
 
-led = LED(2)
-
-while True:
-    led.toggle()
-    sleep(1)
+p = GPIO.PWM(12, 50) 
+p.start(0)   
+try:
+    while 1:
+        for dc in range(0, 101, 5):
+            p.ChangeDutyCycle(dc) 
+            time.sleep(0.1)
+        for dc in range(100, -1, -5):
+            p.ChangeDutyCycle(dc)
+            time.sleep(0.1)
+except KeyboardInterrupt:
+    pass
+p.stop() 
+GPIO.cleanup() 
