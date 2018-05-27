2c0abf56135e8473a60466ad5b6c7862db145f30
ghostlhq <790492245@qq.com>
2018-05-27 20:35:51 +0000

    更新 'crazytest.py'

--- crazytest.py
+++ crazytest.py
@@ -13,10 +13,10 @@ p.start(0)
 time.sleep(2)
 
 
-  for i in range(0,181,10):
-    p.ChangeDutyCycle(2.5 + 10 * i / 180) 
-    time.sleep(0.02)                     
-    p.ChangeDutyCycle(0)              
-    time.sleep(0.2)
-    
+for i in range(0,181,10):
+  p.ChangeDutyCycle(2.5 + 10 * i / 180) 
+  time.sleep(0.02)                     
+  p.ChangeDutyCycle(0)              
+  time.sleep(0.2)
+  
 pause()
