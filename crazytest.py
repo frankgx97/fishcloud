63c25b33d653105f5b1e241fa42ac5f965fa4ceb
ghostlhq <790492245@qq.com>
2018-05-27 16:55:01 +0000

    更新 'crazytest.py'

--- crazytest.py
+++ crazytest.py
@@ -8,15 +8,15 @@ atexit.register(GPIO.cleanup)
 servopin = 21
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(servopin, GPIO.OUT, initial=False)
-p = GPIO.PWM(servopin,50) #50HZ
+p = GPIO.PWM(servopin,50) 
 p.start(0)
 time.sleep(2)
 
 while(True):
   for i in range(0,181,10):
-    p.ChangeDutyCycle(2.5 + 10 * i / 180) #设置转动角度
-    time.sleep(0.02)                      #等该20ms周期结束
-    p.ChangeDutyCycle(0)                  #归零信号
+    p.ChangeDutyCycle(2.5 + 10 * i / 180) 
+    time.sleep(0.02)                     
+    p.ChangeDutyCycle(0)              
     time.sleep(0.2)
   
   for i in range(181,0,-10):
