426d44d1f64adcb7c7074aacfccc3cad46d39889
ghostlhq <790492245@qq.com>
2018-05-27 16:49:30 +0000

    更新 'crazytest.py'

--- crazytest.py
+++ crazytest.py
@@ -1,13 +1,26 @@
 import RPi.GPIO as GPIO
 import time
+import signal
+import atexit
 
-GPIO.setmode(GPIO.BOARD)
-GPIO.setup(11, GPIO.OUT)
+atexit.register(GPIO.cleanup)  
 
-while True:
-    GPIO.output(11, 1)
-    time.sleep(1)
-    GPIO.output(11, 0)  
-    time.sleep(1)  
+servopin = 21
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(servopin, GPIO.OUT, initial=False)
+p = GPIO.PWM(servopin,50) #50HZ
+p.start(0)
+time.sleep(2)
 
-GPIO.cleanup()  
+while(True):
+  for i in range(0,181,10):
+    p.ChangeDutyCycle(2.5 + 10 * i / 180) #设置转动角度
+    time.sleep(0.02)                      #等该20ms周期结束
+    p.ChangeDutyCycle(0)                  #归零信号
+    time.sleep(0.2)
+  
+  for i in range(181,0,-10):
+    p.ChangeDutyCycle(2.5 + 10 * i / 180)
+    time.sleep(0.02)
+    p.ChangeDutyCycle(0)
+    time.sleep(0.2)
