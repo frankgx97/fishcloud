d8ec3c6cb58ad943c8d55f2069728545d0ce53a6
Ligh7ninG <lt1118@live.cn>
2018-05-28 10:07:06 +0000

    更新 'temp_alarm.py'
    typo & indent error

--- temp_alarm.py
+++ temp_alarm.py
@@ -5,25 +5,27 @@ CRIT_TEMP_LOW = 20
 CRIT_TEMP_HIGH = 24
 LISTEN_PERIOD = 1
 
+
 def set_temp_range(low, high):
-	CRIT_TEMP_LOW = low
-	CRIT_TEMP_HIGH = high
+    CRIT_TEMP_LOW = low
+    CRIT_TEMP_HIGH = high
+
 
 def set_listen_period(timeperiod):
-	LISTEN_PERIOD = timeperiod
+    LISTEN_PERIOD = timeperiod
+
 
 def temp_range():
-	current_temp = read_temp()
-	if current_temp > CRIT_TEMP_HIGH:
-		state = 'Too high'
-	else if current_temp < CRIT_TEMP_LOW:
-      	state = 'Too low'
+    current_temp = read_temp()
+    if current_temp > CRIT_TEMP_HIGH:
+        state = 'Too high'
+    elif current_temp < CRIT_TEMP_LOW:
+        state = 'Too low'
     else:
-    	state = 'Normal'
+        state = 'Normal'
     return state, current_temp
-  
+
 while True:
-	print temp_range()
-	time.sleep(LISTEN_PERIOD)
-	
-	
+    print temp_range()
+    time.sleep(LISTEN_PERIOD)
+
