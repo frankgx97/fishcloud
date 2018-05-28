5abbc1b5fd0852c0541ffd9ff89f13565910741c
Ligh7ninG <lt1118@live.cn>
2018-05-28 10:00:14 +0000

    添加 'temp_alarm.py'
    temp_alarm first commit

---
+++ temp_alarm.py
@@ -0,0 +1,29 @@
+import time
+import temperature.py
+
+CRIT_TEMP_LOW = 20
+CRIT_TEMP_HIGH = 24
+LISTEN_PERIOD = 1
+
+def set_temp_range(low, high):
+	CRIT_TEMP_LOW = low
+	CRIT_TEMP_HIGH = high
+
+def set_listen_period(timeperiod)
+	LISTEN_PERIOD = timeperiod
+
+def temp_range():
+	current_temp = read_temp()
+	if current_temp > CRIT_TEMP_HIGH:
+		state = 'Too high'
+	else if current_temp < CRIT_TEMP_LOW:
+      	state = 'Too low'
+    else:
+    	state = 'Normal'
+    return state, current_temp
+  
+while True:
+  	print temp_range()
+    time.sleep(LISTEN_PERIOD)
+	
+	
