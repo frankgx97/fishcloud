7d2b8b9c72a02dec2417824febff48edc898979d
ghostlhq <790492245@qq.com>
2018-05-28 09:59:14 +0000

    更新 'light.py'

---
+++ light.py
@@ -0,0 +1,22 @@
+from gpiozero import LED
+from time import sleep
+
+led = LED(2)
+
+def turn_on():
+  led.on()
+  return led.is_lit
+
+def turn_off():
+  led.off()
+  return led.is_lit
+
+def auto_control():
+  ############not finished
+  return 
+
+while True:
+  print(turn_on())
+  sleep(1)
+  print(turn_off())
+  sleep(1)
