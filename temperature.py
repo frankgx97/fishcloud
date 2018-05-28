aa1d4a375647ac5870a57c6c927633265b171552
ghostlhq <790492245@qq.com>
2018-05-28 09:10:24 +0000

    更新 'temperature.py'

--- temperature.py
+++ temperature.py
@@ -24,8 +24,8 @@ def read_temp():
     if equals_pos != -1:
         temp_string = lines[1][equals_pos+2:]
         temp_c = float(temp_string) / 1000.0
-        temp_f = temp_c * 9.0 / 5.0 + 32.0
-        return temp_c, temp_f
+        #temp_f = temp_c * 9.0 / 5.0 + 32.0
+        return temp_c #, temp_f
   
 while True:
     print(read_temp())   
