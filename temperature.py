80d6cdfb2fb22f398640c49aa3ea08481eb6a8bc
Ligh7ninG <lt1118@live.cn>
2018-05-29 02:20:34 +0000

    更新 'temperature.py'
    全局变量及set函数移至temperature.py

--- temperature.py
+++ temperature.py
@@ -1,6 +1,19 @@
 import os
 import glob
 import time
+
+CRIT_TEMP_LOW = 20
+CRIT_TEMP_HIGH = 30
+LISTEN_PERIOD = 1
+
+def set_temp_range(low, high):
+  CRIT_TEMP_LOW = low
+  CRIT_TEMP_HIGH = high
+
+
+def set_listen_period(timeperiod):
+  LISTEN_PERIOD = timeperiod
+  
   
 os.system('modprobe w1-gpio')
 os.system('modprobe w1-therm')
