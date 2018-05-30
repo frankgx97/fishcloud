350013ee64ad9a59f343d8794c5607c12ef1e66a
ghostlhq <790492245@qq.com>
2018-05-30 06:40:10 +0000

    更新 'tinyservo.py'

--- tinyservo.py
+++ tinyservo.py
@@ -2,12 +2,12 @@ from gpiozero import Servo
 from time import sleep
 
 servo = Servo(21)
-servo.min()
+servo.max()
 
 def feed_fish():
-  servo.max()
-  sleep(1)
   servo.min()
+  sleep(1)
+  servo.max()
   return servo.value
 
 if __name__ == '__main__': 
